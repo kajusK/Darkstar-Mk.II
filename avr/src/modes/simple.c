07b078621ba45742591959fd56da15a50f661af2
Jakub Kaderka <jakub.kaderka@gmail.com>
2017-03-10 10:55:16 +0100

    Refractored simple mode

--- avr/src/modes/simple.c
+++ avr/src/modes/simple.c
@@ -2,7 +2,7 @@
  * Simple flashlight control mode
  *
  * Holding UP button turns headlamp on/off, pressing UP button browses
- * FLOOD led levels, DOWN controls SPOT levels
+ * FLOOD led levels, DOWN controls SPOT levels. Long DOWN press toggles red LED
  *
  * For license, see LICENSE.txt
  *
@@ -13,16 +13,26 @@
 #include "buttons.h"
 #include "light.h"
 
-uint8_t led1_mode;
-uint8_t led2_mode;
-uint8_t timer;
+struct s_mode {
+	uint8_t spot;
+	uint8_t flood;
+	uint8_t red;
+};
+
+static struct s_mode leds;
+static struct s_mode leds_save;
+
+static uint8_t timer;
+static uint8_t hold_done;
+
+static uint8_t in_sleep;
 
 #define BUTTON_UP BUTTON1
 #define BUTTON_DOWN BUTTON2
 
 //time in tenths of ms to be considered as holding
 #define HOLD_TIME 100
-//if both leds are of for more than 2s, turn off
+//if all leds are of for more than 2s, turn off
 #define OFF_TIME 200
 
 #define LIGHT_STEPS 5
@@ -35,8 +45,11 @@ uint8_t light_output[LIGHT_STEPS] = {0, 1, 50, 150, 200};
  */
 void init(void)
 {
-	led1_mode = 0;
-	led2_mode = 0;
+	leds.spot = 0;
+	leds.flood = 0;
+	leds.red = 0;
+	hold_done = 0;
+	in_sleep = 1;
 	timer = 0;
 }
 
@@ -47,43 +60,66 @@ void init(void)
  */
 void loop(void)
 {
+	//Control RED led
+	if (button_pressed_time(BUTTON_DOWN) >= HOLD_TIME &&
+	    button_state(BUTTON_DOWN) == BUTTON_PRESSED && !hold_done) {
+			hold_done = 1;
+			leds.red = leds.red % 2;
+			if (leds.red != 0)
+				in_sleep = 0;
+	}
+
 	//will be turning off
-	if (button_state(BUTTON_UP) == BUTTON_PRESSED &&
-	    button_pressed_time(BUTTON_UP) >= HOLD_TIME) {
-		light_off();
-		return;
-	}
+	if (button_pressed_time(BUTTON_UP) >= HOLD_TIME &&
+	    button_state(BUTTON_UP) == BUTTON_PRESSED && !hold_done) {
+		hold_done = 1;
 
-	if (button_state(BUTTON_UP) == BUTTON_JUST_RELEASED) {
-		//turn off, holding to long
-		if (button_pressed_time(BUTTON_UP) > HOLD_TIME) {
-			power_off(BUTTON_UP);
-			if (led1_mode == 0 && led2_mode == 0) {
-				led1_mode = 1;
-			}
-			light_set(LED_FLOOD, led1_mode, MODE_NORMAL);
-			light_set(LED_SPOT, led2_mode, MODE_NORMAL);
-			return;
+		if (in_sleep) {
+			in_sleep = 0;
+			leds = leds_save;
+			if (leds.red == 0 && leds.spot == 0 && leds.flood == 0)
+				leds.flood = 1;
+		} else {
+			in_sleep = 1;
+
+			//save current settings
+			leds_save = leds;
+			leds.flood = 0;
+			leds.spot = 0;
+			leds.red = 0;
 		}
-
-		led1_mode = (led1_mode + 1) % LIGHT_STEPS;
 	}
 
-	if (button_state(BUTTON_DOWN) == BUTTON_JUST_RELEASED) {
-		led2_mode = (led2_mode + 1) % LIGHT_STEPS;
+	if (!in_sleep) {
+		if (button_state(BUTTON_UP) == BUTTON_JUST_RELEASED &&
+		    button_pressed_time(BUTTON_UP) <= HOLD_TIME)
+			leds.spot = (leds.spot + 1) % LIGHT_STEPS;
+
+		if (button_state(BUTTON_DOWN) == BUTTON_JUST_RELEASED &&
+		    button_pressed_time(BUTTON_DOWN) <= HOLD_TIME)
+			leds.flood = (leds.flood + 1) % LIGHT_STEPS;
 	}
 
-	light_set(LED_FLOOD, light_output[led1_mode], MODE_NORMAL);
-	light_set(LED_SPOT, light_output[led2_mode], MODE_NORMAL);
 
-	//turn off lamp if both leds are of for more than OFF_TIME
-	if (led1_mode == 0 && led2_mode == 0) {
-		timer++;
-		if (timer > OFF_TIME) {
-			power_off(BUTTON_UP);
-			led1_mode = 1;
-			light_set(LED_FLOOD, led1_mode, MODE_NORMAL);
-			timer = 0;
+	light_set(LED_FLOOD, light_output[leds.flood], MODE_NORMAL);
+	light_set(LED_SPOT, light_output[leds.spot], MODE_NORMAL);
+	light_set(LED_RED, leds.red, MODE_NORMAL);
+
+	if (button_state(BUTTON_DOWN) == BUTTON_RELEASED &&
+	    button_state(BUTTON_UP) == BUTTON_RELEASED) {
+		hold_done = 0;
+		//turn off lamp if both leds are of for more than OFF_TIME
+		if (leds.flood == 0 && leds.spot == 0 && leds.red == 0) {
+			timer++;
+			if (timer > OFF_TIME) {
+				if (in_sleep == 0) {
+					in_sleep = 1;
+					leds_save = leds;
+				}
+				power_off(BUTTON_UP|BUTTON_DOWN);
+				timer = 0;
+				return;
+			}
 		}
 	} else {
 		timer = 0;
