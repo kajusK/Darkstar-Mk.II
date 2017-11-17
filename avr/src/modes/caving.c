dd569816ea82a127693f33381153caca8d63a1ce
Jakub Kaderka <jakub.kaderka@gmail.com>
2017-11-17 16:32:41 +0100

    Reverting cf8523e112e265d334897d2c1916c9e983e853a5

--- avr/src/modes/caving.c
+++ avr/src/modes/caving.c
@@ -384,7 +384,7 @@ static void mode_normal(void)
 		hold_done = 0;
 
 	//apply changes from above
-	if (light_get_blink_finished() || cur_levels == 0) {
+	if (light_get_blink_finished()) {
 		light_set(LED_FLOOD, levels[cur_levels].flood, MODE_NORMAL);
 		light_set(LED_SPOT, levels[cur_levels].spot, MODE_NORMAL);
 		light_set(LED_RED, levels[cur_levels].red, MODE_NORMAL);
